907b1ff5b315f3fb381369d4e861918656c5154d
Salam Sibai <salam.sibai@hotmail.com>
2024-12-30 16:29:00 +0400

    finished  ex02

--- Cpp02/ex01/Fixed.cpp
+++ Cpp02/ex01/Fixed.cpp
@@ -11,12 +11,34 @@ Fixed::Fixed(const Fixed& fixed)
 	*this = fixed;
 }
 
+/**
+ * @brief Construct a new Fixed:: Fixed object
+ * 
+ * 			shifting the decimal point 8 to the right
+ * 
+ * @param i the value of the fixed point in int 
+ * 
+ */
 Fixed::Fixed(const int	i)
 {
-
+	fixed_point = i * (1 << fractional_bits);
 }
 
-Fixed& 		Fixed::operator=(const Fixed &rhs)
+/**
+ * @brief Construct a new Fixed:: Fixed object
+ * 
+ * 			TRUNCATES the value of i to fit into the fixed point
+ * 			if a cast is used, it would more correctly represent
+ * 			the number.
+ * 
+ * @param i the value of the fixed point in int 	
+ */
+Fixed::Fixed(const float i)
+{
+	fixed_point = (i * (1 << fractional_bits));
+}
+
+Fixed&	Fixed::operator=(const Fixed &rhs)
 {
 	this->fixed_point = rhs.getRawBits();
 	return (*this);
@@ -32,10 +54,23 @@ void Fixed::setRawBits(const int raw)
 	this->fixed_point = raw;
 }
 
+float	Fixed::toFloat() const
+{
+	return (fixed_point / 256.0f);
+}
+
+int	Fixed::toInt() const
+{
+	return (fixed_point / 256);
+}
+
 Fixed::~Fixed()
 {
 	std::cout << "destructor called" << std::endl;
 }
 
-
-
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed)
+{
+	os << fixed.toFloat();
+	return os;
+}
--- Cpp02/ex01/Fixed.h
+++ Cpp02/ex01/Fixed.h
@@ -16,7 +16,7 @@ class Fixed
 {
 	private:
 		int					fixed_point;
-		static const int 	fractional_bit = 8;
+		static const int 	fractional_bits = 8;
 	public:
 		Fixed();
 		Fixed(const Fixed& fixed);
